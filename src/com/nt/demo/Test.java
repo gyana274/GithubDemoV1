42ca0544575232697fba665c4b2b15f8e4805f7c
gyana <gyana@DESKTOP-TVPRDFB>
2021-03-30 14:13:51 +0530

    first commit....

---
+++ src/com/nt/demo/Test.java
@@ -0,0 +1,19 @@
+package com.nt.demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String s="hungry";
+		
+		if(s.equalsIgnoreCase("HUNGRY"))
+		{
+			System.out.println("He is going to eat");
+		}
+		else
+		{
+			System.out.println("He is not going to eat now.....");
+		}
+
+	}
+
+}
